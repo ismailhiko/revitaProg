9d8d92f7cf232bf5c97ce0e405d271ec19d8e1ea
Ismail HAMAIMOU <ishamaimou@gmail.com>
2024-11-12 10:43:27 +0100

    reservation

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideNativeDateAdapter, MAT_DATE_LOCALE } from '@angular/material/core';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    ...appConfig.providers, // Conserve les providers existants dans appConfig
+    provideNativeDateAdapter(), // Fournisseur pour l'adaptateur de date natif
+    { provide: MAT_DATE_LOCALE, useValue: 'fr-FR' } // Facultatif : Définit la langue française pour le datepicker
+  ]
+}).catch((err) => console.error(err));
